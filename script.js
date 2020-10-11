f0d795209567329183feac656440d9cf3f2eda17
Matt Clark <clark.matthewr@gmail.com>
2020-10-11 15:32:16 -0500

    test inline demo of dijsktra

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-var links = document.querySelectorAll("a");
+// window.addEventListener('DOMContentLoaded', function() {
+//   var links = document.querySelectorAll("a");
 
-for (var i = 0; i < links.length; i += 1) {
-  links[i].setAttribute("target", "blank");
-}
+//   for (var i = 0; i < links.length; i += 1) {
+//     links[i].setAttribute("target", "blank");
+//   }
+
+//   let dijkstraDemo = document.getElementById('dijkstra-demo');
+//   dijkstraDemo.addEventListener('click', function(event) {
+//     if (dijkstraDemo.children.length === 0) {
+//       // add iframe
+//       let iframe = 
+//     } else {
+//       // remove iframe
+//     }
+//   });
+// });
+
+$(function() {
+  $('a').attr("target", "blank");
+
+  let $dijkstraDemo = $('#dijkstra-demo');
+  let $dijkstraButton = $('#dijkstra-demo-button');
+  let buttonText = $dijkstraButton.text();
+  $dijkstraButton.click(event => {
+    if ($dijkstraDemo.children().length === 0) {
+      let $iframe = $(document.createElement('iframe'));
+      $iframe.attr({
+        src: "https://matthew-r-clark.github.io/dijkstra_implementation/",
+        frameborder: "0",
+        scrolling: "no",
+      });
+      $dijkstraDemo.append($iframe);
+      $dijkstraButton.text('Close Demo');
+    } else {
+      $dijkstraDemo.empty();
+      $dijkstraButton.text(buttonText);
+    }
+  });
+});
